ab782ba28c76bb15699c95c7190e17ccee55c8c4
Aidan <adssoccer1@gmail.com>
2020-08-19 20:44:16 -0400

    added privacy policy

--- src/App.js
+++ src/App.js
@@ -201,7 +201,10 @@ class App extends Component {
     this.setState({logginPopUp : false});
   }
 
-
+  responseGoogle = (response) => {
+    console.log("sign in failed")
+    console.log(response);
+  }
   
   render() {
 
